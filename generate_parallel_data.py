ccab5a4ed596e67044478a186c80bee5db4a9f49
kttian <19katherinet@students.harker.org>
2021-03-30 14:18:09 -0400

    generate_parallel_data code update

--- generate_parallel_data.py
+++ generate_parallel_data.py
@@ -4,42 +4,25 @@ from utils import set_seed, logging, load_sent, load_sent_no_split, write_sent_n
 
 parser = argparse.ArgumentParser()
 
-''' parser.add_argument('--train', metavar='FILE', required=True,
-                    help='path to training file')
-
-parser.add_argument('--data', metavar='FILE', required=True,
-                    help='path to data folder') '''
-
 tense_data_dir = "data/yelp/tense/"
 parallel_data_dir = "parallel_data/"
 
-def convert(past_flag):
+def convert(tense_idx):
     tenses = ["present", "past"]
-    tense_idx = int(past_flag)
-    
     tense_data = tense_data_dir + "valid." + tenses[tense_idx]
     tense_sents = load_sent_no_split(tense_data)
     converted_sents = []
     for sent in tense_sents:
-        changed = change_tense(sent, 1-tense_idx)
+        changed = change_tense(sent, tenses[1-tense_idx])
         converted_sents.append(changed)
-    return converted_sents
+    return tense_sents, converted_sents
 
 def main(args):
-    tense_data_dir = "data/yelp/tense/"
-    parallel_data_dir = "parallel_data/"
-    past_sents = load_sent_no_split(tense_data_dir + "valid.past")
-    present_sents = []
-    for sent in past_sents:
-        changed = change_tense(sent, 'present')
-        present_sents.append(changed)
-
-    more_present = load_sent_no_split(tense_data_dir + "valid.present")
-    present_sents += more_present
-    for sent in more_present:
-        changed = change_tense(sent, 'past')
-        past_sents.append(changed)
-
+    past_sents, present_sents = convert(1) # converts past to present
+    present2, past2 = convert(0) # converts present to past
+    past_sents += past2
+    present_sents += present2 
+    
     write_sent_no_split(past_sents, parallel_data_dir + "past.txt")
     write_sent_no_split(present_sents, parallel_data_dir + "present.txt")
 

b35812409f8f57170cecd59cbc5033839006764b
kttian <19katherinet@students.harker.org>
2021-06-30 00:29:05 -0400

    evaluate updates

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 from utils import *
 import nltk
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--outputs', metavar='FILE', required=True,
@@ -31,8 +32,8 @@ def classify_sent(sent):
     return classify_tagged(tagged)
 
 #############################################################
-pres_path = "parallel_data/present.txt"
-past_path = "parallel_data/past.txt"
+pres_path = "parallel_data/present_v2.txt"
+past_path = "parallel_data/past_v2.txt"
 
 # pres_path = "data/yelp/tense/valid.present"
 # past_path = "data/yelp/tense/valid.past"
@@ -128,13 +129,63 @@ def get_ppl(outputs_pred):
     ppl = calc_ppl(outputs_pred, 100)
     print("perplexity:", ppl)
 
+import math
+from pytorch_pretrained_bert import OpenAIGPTTokenizer, OpenAIGPTModel, OpenAIGPTLMHeadModel
+# Load pre-trained model (weights)
+model = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt')
+model.eval()
+# Load pre-trained model tokenizer (vocabulary)c
+tokenizer = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
+
+def score(sentence):
+    tokenize_input = tokenizer.tokenize(sentence)
+    tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)])
+    loss=model(tensor_input, lm_labels=tensor_input)
+    return math.exp(loss)
+
+def get_ppl(outputs_pred):
+    # breakpoint()
+    sample = 10
+    scores = [score(x) for x in outputs_pred[:sample]]
+    print("done w scores")
+    base_pres = [score(x) for x in pres_sents[:sample]]
+    print("done pres")
+    base_past = [score(x) for x in past_sents[:sample]]
+    print("done past")
+
+    for i in range(5):
+        print(outputs_pred[i])
+        print(past_sents[i])
+        print(i, ":", scores[i], base_pres[i], base_past[i])
+    
+    perp = sum(scores)/len(scores)
+    perp_pres = sum(base_pres)/len(base_pres)
+    perp_past = sum(base_past)/len(base_past)
+
+    print("perplexity:", perp)
+    print("--perp pres:", perp_pres)
+    print("--perp past:", perp_past)
+
 ### main ###
 def main(args):
-    # get_baselines()
+    # start_time = time.perf_counter()
+    # print(0)
+    get_baselines()
     get_accuracy(args.outputs)
+    # cur_time = time.perf_counter()
+    # print(cur_time - start_time)
+
     outputs_pred = load_sent(args.outputs)
     get_bleu(outputs_pred, args.gt)
-    get_ppl(outputs_pred)
+
+    # cur_time = time.perf_counter()
+    # print(cur_time - start_time)
+    # outputs_pred = load_sent_no_split(args.outputs)
+    # print("hi")
+    # get_ppl(outputs_pred)
+
+    # cur_time = time.perf_counter()
+    # print(cur_time - start_time)
 
 if __name__ == '__main__':
     args = parser.parse_args()
